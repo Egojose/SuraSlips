8f405773629497b4e98ae6f9854c76548c397207
Daniel <danielgiraldoch@gmail.com>
2019-02-11 10:33:58 -0500

    publicaciòn JAM

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  urlWeb:'https://suramericana.sharepoint.com/sites/intranet/negocio/seguros/slips',
-  urlReltativa: '/sites/intranet/negocio/seguros/slips/',
+  urlWeb:'https://enovelsoluciones.sharepoint.com/sites/jam/slips',
+  urlReltativa: '/sites/jam/slips',
   listaDescargaPlantillas: "PlantillasSlips",
   bibliotecaPlantillas: "FormatosSlips",
   listaTipoNegocio: "TipoNegocio",
